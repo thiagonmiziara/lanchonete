c4cbaac1a974ad928216d3dc08e873b2c021c479
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-05 19:41:33 -0300

    add lanchonete

--- js/script.js
+++ js/script.js
@@ -1,57 +1,50 @@
-const $ = document.querySelector.bind(document)
-const hamburguer = $('.hamburguer h1');
-const refrigerantes = $('.refrigerantes h1');
-const sucos = $('.sucos h1');
-const porcoes = $('.porcoes h1');
+const $ = document.querySelector.bind(document);
+const hamburguer = $(".hamburguer h1");
+const refrigerantes = $(".refrigerantes h1");
+const sucos = $(".sucos h1");
+const porcoes = $(".porcoes h1");
 
-hamburguer.addEventListener('click', addClass);
-refrigerantes.addEventListener('click', addClassRefri);
-sucos.addEventListener('click', addClassSucos);
-porcoes.addEventListener('click', addClassPorcoes);
+hamburguer.addEventListener("click", addClass);
+refrigerantes.addEventListener("click", addClassRefri);
+sucos.addEventListener("click", addClassSucos);
+porcoes.addEventListener("click", addClassPorcoes);
 
 function addClass(event) {
-    const submenu = document.querySelectorAll('.submenu-burguer');
+    const submenu = document.querySelectorAll(".submenu-burguer");
     event = submenu.forEach((item) => {
-        item.classList.toggle('ativo');
-    })
-
+        item.classList.toggle("ativo");
+    });
 }
 
 function addClassRefri(event) {
-    const submenu = document.querySelectorAll('.submenu-refri');
+    const submenu = document.querySelectorAll(".submenu-refri");
     event = submenu.forEach((item) => {
-        item.classList.toggle('ativo');
-    })
-
+        item.classList.toggle("ativo");
+    });
 }
 
 function addClassSucos(event) {
-    const submenu = document.querySelectorAll('.submenu-sucos');
+    const submenu = document.querySelectorAll(".submenu-sucos");
     event = submenu.forEach((item) => {
-        item.classList.toggle('ativo');
-    })
-
+        item.classList.toggle("ativo");
+    });
 }
 
 function addClassPorcoes(event) {
-    const submenu = document.querySelectorAll('.submenu-porcoes');
+    const submenu = document.querySelectorAll(".submenu-porcoes");
     event = submenu.forEach((item) => {
-        item.classList.toggle('ativo');
-    })
-
+        item.classList.toggle("ativo");
+    });
 }
 
-
-//////////////////////////////////modal////////////////////
-
-const btnPedido = $('.btn-pedido');
-const btnFecharModal = $('.fechar-modal');
-const containerModal = $('.modal-container');
+const btnPedido = $(".btn-pedido");
+const btnFecharModal = $(".fechar-modal");
+const containerModal = $(".modal-container");
 
 if (btnPedido && btnFecharModal && containerModal) {
     function toggleModal(event) {
         event.preventDefault();
-        containerModal.classList.toggle('active');
+        containerModal.classList.toggle("active");
     }
 
     function cliqueForaModal(event) {
@@ -59,30 +52,32 @@ if (btnPedido && btnFecharModal && containerModal) {
             toggleModal(event);
         }
     }
-    btnPedido.addEventListener('click', toggleModal);
-    btnFecharModal.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
-
+    btnPedido.addEventListener("click", toggleModal);
+    btnFecharModal.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", cliqueForaModal);
 }
 
-
-///////////////
-
-const btnMais = document.querySelectorAll('li .btnMais');
+const btnMais = document.querySelectorAll("li .btnMais");
 btnMais.forEach((item) => {
-    item.addEventListener('click', adicionar)
-})
+    item.addEventListener("click", adicionar);
+});
 
 function adicionar(event) {
-    console.log('Adicionou')
+    console.log("Adicionou");
 }
 
-const btnMenos = document.querySelectorAll('li .btnMenos');
+const btnMenos = document.querySelectorAll("li .btnMenos");
 btnMenos.forEach((item) => {
-    item.addEventListener('click', remover);
-
-})
+    item.addEventListener("click", remover);
+});
 
 function remover(event) {
-    console.log('Removeu')
+    console.log("Removeu");
+}
+
+const btnConfirmar = $('.btn-confirmar');
+btnConfirmar.addEventListener('click', confirmou);
+
+function confirmou() {
+    alert('Pedido enviado com sucesso!!')
 }
