1f8ee55871f8a1c4e60277d221f8f139c7a895fb
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-05 22:14:41 -0300

    add lanchonete

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const refrigerantes = $(".refrigerantes h1");
 const sucos = $(".sucos h1");
 const porcoes = $(".porcoes h1");
 
+
+
 hamburguer.addEventListener("click", addClass);
 refrigerantes.addEventListener("click", addClassRefri);
 sucos.addEventListener("click", addClassSucos);
@@ -66,8 +68,10 @@ const cardapio = [
     { name: 'X-salada', value: 10.00 },
     { name: 'X-bacon', value: 15.00 },
     { name: 'X-tudo', value: 25.00 },
-    { name: 'Suco', value: 7.5 },
-    { name: 'Refrigerante', value: 7.5 },
+    { name: 'Suco', value: 7.00 },
+    { name: 'Refrigerante', value: 6.00 },
+    { name: 'Porções', value: 30.00 },
+
 ];
 
 const carrinho = [];
@@ -76,12 +80,12 @@ function atualizarCarrinho() {
     carrinhoItemsListElement.innerHTML = '';
     carrinho.forEach(item => {
         const itemCarrinho = document.createElement('div');
-        itemCarrinho.classList.add('container-flex')
+        itemCarrinho.classList.add('container-flex');
         itemCarrinho.textContent = `${item.name} - R$ ${item.value},00`;
         carrinhoItemsListElement.appendChild(itemCarrinho);
     });
     const total = carrinho.length ? carrinho.reduce((a, b) => ({ value: a.value + b.value })).value : 0;
-    totalElement.textContent = `${total},00`;
+    totalElement.textContent = `R$${total},00`;
 }
 
 
@@ -107,7 +111,7 @@ cardapio.forEach(item => {
     btnItemSub.classList.add('btnMenos')
     btnItemSub.onclick = () => removeItem({...item });
     const itemContainer = document.createElement('div');
-    itemContainer.classList.add('container-flex')
+    itemContainer.classList.add('container-flex');
     itemContainer.textContent = item.name;
     itemContainer.appendChild(btnItemAdd);
     itemContainer.appendChild(btnItemSub);
